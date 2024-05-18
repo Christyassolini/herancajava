44a2016c4fade8b689c5e14d5bdd907ebb49b905
CHRISTYAN ASSOLINI <christyan.assolini@univille.br>
2024-05-17 21:38:16 -0300

    herança

---
+++ src/Funcionario.java
@@ -0,0 +1,39 @@
+public class Funcionario {
+    private String nome;
+    private double valorHora;
+    private int horasTrabalhadas;
+
+    public Funcionario(String nome, double valorHora, int horasTrabalhadas) {
+        this.nome = nome;
+        this.valorHora = valorHora;
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public double calcularSalario() {
+        return valorHora * horasTrabalhadas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+}
